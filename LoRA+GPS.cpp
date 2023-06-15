706d56edd72993c876b7271f9ecd6064bf5dde7a
ufocet1 <110926327+ufocet1@users.noreply.github.com>
2023-06-15 22:56:24 +0300

    Update LoRA+GPS.cpp
    Fixed syntax and a few issues that prevented it from working as intended.

--- LoRA+GPS.cpp
+++ LoRA+GPS.cpp
@@ -1,115 +1,127 @@
-//Include necessary libraries for LoRa, Arduino, and the GPS module. 
+// Include necessary libraries
 #include <SPI.h>
 #include <LoRa.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <EEPROM.h>
 
-//Pin Definitions
-int RXPin = 2; // Connect GPS module's RX pin to Arduino pin 2 (RX)
-int TXPin = 3; // Connect GPS module's TX pin to Arduino pin 3 (TX)
+// Pin Definitions
+int RXPin = 5; // Connect GPS module's RX pin to Arduino pin 5 (RX) (GREEN)
+int TXPin = 6; // Connect GPS module's TX pin to Arduino pin 6 (TX) (YELLOW)
 
-//Create GPS object, establish GPS serail communication.
+// Create GPS object and establish GPS serial communication
 TinyGPSPlus gps;
 SoftwareSerial SoftSerial(RXPin, TXPin);
 
-//Declare variables
+// Declare variables
 int count = 0;
-int EEPROM_Adress = 0;
+int EEPROM_Address = 0;
 
-//Create GPS function which will be used to get info from the GPS
-void GPS(){
-  
-  //Declare GPS variables.
-  
-  double Lat;
-  double Lng;
-  int Hour;
-  int Minute;
-  int Second;
-  int Satellite;
-  
-  
-  //Check if the GPS is available and sending DATA
-  while(SoftSerial.avaliable() > 0){
-    //Encode GPS lines using GPS++ Library
-    if(gps.encode(SoftSerial.read())){
-     
-      //If GPS location is updated send the location using LoRa and write data on to EEPROM.
-      //In order to avoid EEPROM memory degredation, EEPROM write cycle is only initiated after 2 minutes have already passed.
-      //If after 2 minutes the location is updated the new location is written onto the EEPROM.
-      
+// GPS data variables
+double Lat;
+double Lng;
+int Hour;
+int Minute;
+int Second;
+int Satellite;
+
+// Create GPS function to retrieve information from the GPS
+void GPS() {
+  // Check if the GPS is available and sending data
+  while (SoftSerial.available() > 0) {
+    // Encode GPS lines using GPS++ Library
+    if (gps.encode(SoftSerial.read())) {
+      // If GPS location is updated, send the location using LoRa and write data to EEPROM.
       if (gps.location.isUpdated()) {
-        
-        Lat = (gps.location.lat(),6);
-        Lng = (gps.location.lng(),6);
-        Hour = (gps.time.hour());
-        Minute = (gps.time.minute());
-        Second = (gps.time.second());
-        Satellite = (gps.satellites.value());
-        
-        //Parse everything into a single variable for EEPROM.
-        
-        String datGPS = "Lat:" + String(Lat) + " Lng:" + String(Lng) + " Hour:" + String(Hour) + " Minute:" + String(Minute) + " Second:" + String(Second) + " Satellites:" + String(Satellite); 
-        
-        //Check if 2 minutes has passed, if so write and reset timer.
-        
-        if(count > 120){
-          EEPROM.write(EEPROM_Adress,datGPS);
+        Lat = gps.location.lat();
+        Lng = gps.location.lng();
+        Hour = gps.time.hour();
+        Minute = gps.time.minute();
+        Second = gps.time.second();
+        Satellite = gps.satellites.value();
+
+        // Construct GPS data string
+        String gpsData = "Lat:" + String(Lat, 6) +
+                         " Lng:" + String(Lng, 6) +
+                         " Hour:" + String(Hour) +
+                         " Minute:" + String(Minute) +
+                         " Second:" + String(Second) +
+                         " Satellites:" + String(Satellite);
+
+        // Check if 2 minutes have passed, if so write to EEPROM and reset the timer.
+        if (count > 120) {
+          int dataSize = gpsData.length() + 1;
+          for (int i = 0; i < dataSize; i++) {
+            EEPROM.write(EEPROM_Address + i, gpsData[i]);
+          }
+
+          Serial.println("EEPROM Updated!"); // Debug line
+
+          LoRa.beginPacket();
+          LoRa.println("EEPROM Updated!");
+          LoRa.endPacket();
+
           count = 0;
         }
-        //Send packet to LoRa
+
+        // Debug lines
+        Serial.println(gpsData);
+
+        // Send packet using LoRa
         LoRa.beginPacket();
-        LoRa.println(datGPS);
+        LoRa.println(gpsData);
         LoRa.endPacket();
-    }  
+      }
+    }
   }
-    
-  //FAILSAFE METHOD
-  //IF the GPS is somehow not able to get a fix properly, send the last known location using data written on the EEPROM. 
-  while(SoftSerial.avaliable() == 0){
-    //Send Last Packet to LoRa
-    LoRa.beginPacket();
-    LoRa.println((EEPROM.read(EEPROM_Adress)));
-    LoRa.endPacket();
-  }  
+
+  // Failsafe method: If the GPS is unable to get a fix, send the last known location using data from EEPROM.
+  String lastGPS;
+  char dataChar;
+  int i = 0;
+
+  while (dataChar = EEPROM.read(EEPROM_Address + i)) {
+    lastGPS += dataChar;
+    i++;
+  }
+
+
+  Serial.println("Failsafe Active!, Last Known Location:  " + lastGPS); //Debug Lines
+  
+  // Send last packet to LoRa
+  LoRa.beginPacket();
+  LoRa.println("Failsafe Active!, Last Known Location:  " + lastGPS);
+  LoRa.endPacket();
 }
+
 
 void setup() {
   Serial.begin(9600);
   SoftSerial.begin(9600);
-  
-  while (!Serial);
+
+  while (!Serial)
+    ;
 
   Serial.println("This is the Sender (GPS Attached) Module");
 
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
-  
+
   LoRa.setTxPower(20);
-  
-  //Notify the reciever
+
+  // Notify the receiver
   LoRa.beginPacket();
   LoRa.print("Wireless Connection Established");
   LoRa.endPacket();
-  
 }
 
-void loop(){
-  
+void loop() {
   GPS();
   delay(1000);
-  
-  //Count 120 seconds
+
+  // Count 1 second
   count++;
-  
 }
-
-
-
-
-
-
-
